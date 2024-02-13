bdf1b7d51afab172406cfb16d6d04accbca29f81
mtalhastar <mtalhastar@gmail.com>
2024-02-13 23:27:03 +0500

    done

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -11,6 +13,20 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mr Bashir',
+          style: GoogleFonts.montserrat(color: Colors.black, fontSize: 20),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+      ),
+      body: SizedBox()
+       
+    );
   }
 }
